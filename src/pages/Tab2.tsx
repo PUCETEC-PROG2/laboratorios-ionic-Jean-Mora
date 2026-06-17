e5a655227a32e92f90ae2439632976250a4edb06
Jean-Mora <mora5252@hotmail.com>
2026-06-17 15:54:37 -0500

    Laboratorio 7

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,5 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
+import { IonButton, IonContent, IonHeader, IonInput, IonPage, IonTextarea, IonTitle, IonToolbar } from '@ionic/react';
 import './Tab2.css';
 
 const Tab2: React.FC = () => {
@@ -7,19 +6,47 @@ const Tab2: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Tab 2</IonTitle>
+          <IonTitle>Formulario de Repositorio</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Formulario de Repositorio</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+
+        <div className="form-container">
+          <IonInput
+             className="form-field"
+             label="Nombre del Repositorio"
+             labelPlacement="floating"
+             placeholder="Ingrese nombre del Repositorio"
+          />
+          <IonTextarea
+             className="form-field"
+             label="Descripcion del Repositorio"
+             labelPlacement="floating"
+             placeholder="Ingrese la descripcion del Repositorio"
+             rows={6}
+          />
+          <IonButton
+             className="form-field"
+             expand="block"
+             shape="round"
+             color="primary"
+          >
+             Guardar
+
+          </IonButton>
+          
+        </div>
+        
+
       </IonContent>
     </IonPage>
   );
 };
 
 export default Tab2;
+
